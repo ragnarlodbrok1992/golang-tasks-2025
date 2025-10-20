f6a9d00e67398e42c8bc6c1b18f8d41f37fc9cad
Maciej Oliwa <maraus92@gmail.com>
2025-10-20 21:23:44 +0200

    Parsing tokens

--- 001-gorth/gorth.go
+++ 001-gorth/gorth.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	// "encoding/hex"
 	// "strings"
 )
 
+const ASCII_SPACE = 0x20
+const ASCII_NEWLINE = 0x0A
+
 type ErrorCode int
+type Token int
+type ParseControl int
 
 const (
 	_ ErrorCode = iota // Skipping 0
@@ -14,6 +20,20 @@ const (
 	ErrWrongSourceFile
 	ErrSourceFileStats
 	ErrLoadingBuffer
+)
+
+const (
+	TokenMain Token = iota // Special token - entry function
+	TokenInt
+	TokenFunc
+	TokenOp
+	TokenComment
+	TokenEnd // Stop keyword
+	TokenEOF // Keep last
+)
+
+const (
+	ParseControlNoMoreTokens ParseControl = -1
 )
 
 var asm_empty_program = `
@@ -26,13 +46,47 @@ _start:
 	syscall
 `
 
+// Creating a closure that will keep the source file data
+// and return tokens until it is empty
+func NewTokenParser(source_code_buffer []byte) func() (Token, string) { // Probably will have to return pair of Token and value as string
+	parser_buffer := make([]byte, len(source_code_buffer))
+	copy(parser_buffer, source_code_buffer)
+	current_index := 0 // Start at beginning
+
+	return func() (Token, string) {
+		var token_buffer []byte
+
+		// // Right now returning source file bytes - characters
+		// cur_char := parser_buffer[current_index]
+		// current_index++
+		// // fmt.Println(cur_char)
+		// return cur_char 
+
+		for {
+			if current_index + 1 > len(parser_buffer) {
+				return TokenEOF, "EOF"
+			}
+
+			cur_byte := parser_buffer[current_index];
+			// fmt.Println("cur_byte --> 0x", hex.EncodeToString([]byte{cur_byte}))
+
+			if cur_byte == ASCII_SPACE || cur_byte == ASCII_NEWLINE {
+				// fmt.Println("Encountered ASCII_SPACE or ASCII_NEWLINE!")
+				ret_buffer := token_buffer
+				token_buffer = token_buffer[:0] // Idk if this is how you clean the byte buffer
+				current_index++
+				return TokenOp, string(ret_buffer) // For now - it's getting quite complicated real fast
+			} else {
+				token_buffer = append(token_buffer, cur_byte)
+				current_index++
+			}
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Hello, gorth!")
 	defer fmt.Println("Goodbye...")
-
-	// Test prints
-	fmt.Println("Some test prints:")
-	fmt.Println(asm_empty_program)
 
 	// Opening input file
 	for i := 1; i < len(os.Args); i++ {
@@ -47,7 +101,6 @@ func main() {
 
 				if i + 1 < len(os.Args) {
 					src_file := os.Args[i + 1]
-					fmt.Println("src_file: ", src_file)
 
 					// Trying to open the source file
 					file, err := os.Open(src_file)
@@ -60,27 +113,28 @@ func main() {
 						fmt.Println("Couldn't get file stats!")
 						os.Exit(int(ErrSourceFileStats))
 					}
-					// File opened correctly - do something with it
-					// Write out information about the file
-					// Test code
-					fmt.Printf("Name: %s\n", file.Name()) 
-					fmt.Printf("Size: %d bytes\n", file_information.Size()) 
 
 					// Allocate array of chars
 					source_file_data := make([]byte, file_information.Size())
 
-					// Populate allocated memory
 					n, err := file.Read(source_file_data)
 					if err != nil {
 						fmt.Println("Couldn't populate src file buffer!")
 						os.Exit(int(ErrLoadingBuffer))
 					}
-					fmt.Println("Read up to ", n)
+					fmt.Println("Source file is ", n, " bytes")
 
-					// Print out source file
-					fmt.Println("--- source file ---")
-					fmt.Println(string(source_file_data))
-					fmt.Println("--- end of source file ---")
+					// Move to code parsing function
+					parser := NewTokenParser(source_file_data) // We return function here
+
+					for {
+						next_token, token_value := parser()
+						if next_token == TokenEOF {
+							break
+						}
+						// Write out char of source file
+						fmt.Println(string(next_token), token_value)
+					}
 
 					defer file.Close()
 				} else {
